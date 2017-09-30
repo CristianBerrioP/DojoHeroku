d8296d05aaf76869d740d45dd2619783c11014d1
Reizzel <cbp453252.hdrl@gmail.com>
2017-09-30 08:38:21 -0500

    Inicio del app

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <h1>My first PHP page</h1>
+
+    <?php
+      echo "Hello App Empresariales - Cristian Berrio"
+     ?>
+     
+  </body>
+</html>
